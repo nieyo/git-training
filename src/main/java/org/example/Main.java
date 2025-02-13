291d48cd4e509a4548df7122cae48e0af8b494da
nieyo <44291594+nieyo@users.noreply.github.com>
2025-02-13 11:41:21 +0100

    Alter: hello world (#1)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello,");
+        System.out.println("World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
     }
